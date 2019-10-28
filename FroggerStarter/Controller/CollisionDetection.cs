85aaacc2199f6988192394e80cc2005156b0156f
Nathaniel Lightholder <nlighth1@my.westga.edu>
2019-10-28 19:01:15 -0400

    Added collision detection class to handle different types of collision such as frog on frog home

--- FroggerStarter/Controller/CollisionDetection.cs
+++ FroggerStarter/Controller/CollisionDetection.cs
@@ -8,12 +8,58 @@ using FroggerStarter.Model;
 
 namespace FroggerStarter.Controller
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class CollisionDetection
     {
+        private readonly Frog player;
+        private readonly Collection<FrogHome> homes;
+        private readonly RoadManager lanes;
 
-        public CollisionDetection(Frog player, Collection<FrogHome> homes, Collection<Lane> lanes)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CollisionDetection"/> class.
+        /// </summary>
+        /// <param name="player">The player.</param>
+        /// <param name="homes">The homes.</param>
+        /// <param name="lanes">The lanes.</param>
+        public CollisionDetection(Frog player, Collection<FrogHome> homes, RoadManager lanes)
         {
+            this.player = player;
+            this.homes = homes;
+            this.lanes = lanes;
 
+        }
+
+        /// <summary>
+        /// Checks for vehicle on player collision.
+        /// </summary>
+        /// <returns></returns>
+        public bool CheckForVehicleOnPlayerCollision()
+        {
+            foreach (Lane lane in this.lanes)
+            {
+                foreach (Vehicle vehicle in lane)
+                {
+                    return this.player.BoundingBox.IntersectsWith(vehicle.BoundingBox);
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Checks for player on frog home collision.
+        /// </summary>
+        /// <returns></returns>
+        public bool CheckForPlayerOnFrogHomeCollision()
+        {
+            foreach (var home in this.homes)
+            {
+                return this.player.BoundingBox.IntersectsWith(home.BoundingBox);
+            }
+
+            return false;
         }
     }
 }
